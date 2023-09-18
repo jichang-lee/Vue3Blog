0af3605699eb3d0a68e9e570597435c1556849c0
dlwlckdWkd12 <jccc23@naver.com>
2023-09-18 22:12:06 +0900

    frame

---
+++ src/components/BlogList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-for="(blog , index) in data" :key="index">
+        <h5>{{blog[0].title}}</h5>
+        
+        <p>{{blog[0].content}} </p>    
+    </div>    
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const data = defineProps({
+  data: {
+    Type: Array,
+   default: () => ({}),
+  },
+})
+
+</script>
